cf77dec5ce5ed0dade406746c6a686a7f72ec45d
user.name <user.email>
2020-04-13 10:03:27 +0700

    V 1.2

---
+++ src/TranVanTy_59133029_BT2_Tuan2/IQuanLy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TranVanTy_59133029_BT2_Tuan2;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface IQuanLy {
+    
+}
